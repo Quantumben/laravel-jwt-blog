82d9c6bfa353ec40f5a8b3e01481bdb7661c1e2e
QuantumBen <omosabenedict@gmail.com>
2023-09-20 11:52:07 +0100

    Updated the user Model to fix error

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,8 +9,7 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
-
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -54,5 +53,4 @@ class User extends Authenticatable
     {
       return [];
     }
-
 }
